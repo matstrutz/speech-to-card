580cb199c93ddccfc67b2134e5523c6872c19957
matstrutz <tuffaogamer@hotmail.com>
2022-12-02 10:16:08 -0300

    feat: start project

---
+++ src/main/java/com/speech/card/reader/Application.java
@@ -0,0 +1,13 @@
+package com.speech.card.reader;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/speech/card/reader/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.speech.card.reader;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
